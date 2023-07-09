54df66d9f777d9bb064b2321b68780f075dd0d4a
John Nweke <johnnweke@users.noreply.github.com>
2023-07-08 21:49:45 -0400

    Updated Maven shell script

--- maven_quickstart.sh
+++ maven_quickstart.sh
@@ -1,2 +1,6 @@
- mvn archetype:generate -DgroupId=com.evangel -DartifactId=passport-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4
+# /bin/bash
+
+# Maven Quick Start Shell Script Template
+
+mvn archetype:generate -DgroupId=com.evangel -DartifactId=evangel-passport-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4
 s
